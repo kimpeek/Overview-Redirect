fbb116aec9a708ad9bf66af900571b5cf90f0e5f
KimPeek <kimpeek@users.noreply.github.com>
2017-07-19 13:51:09 -0500

    Create overview_redirect.user.js

---
+++ overview_redirect.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Overview Redirect
+// @name:en      Overview Redirect
+// @version      0.2
+// @description  Change all username urls to point to overviews instead of profiles
+// @namespace
+// @author       https://github.com/kimpeek
+// @include      https://www.reddit.com*
+// ==/UserScript==
+
+var user_link = new RegExp("(\/u\/|\/user\/)[A-Za-z0-9_-]+$");
+var anchors = document.getElementsByTagName("a");
+
+for (var i = 0; i < anchors.length; i++) {
+    if (user_link.test(anchors[i].href)){
+        var user = anchors[i].href.match(user_link)[0];
+        var overview = "https://www.reddit.com" + user + "/overview";
+        anchors[i].href = overview;
+    }
+}
