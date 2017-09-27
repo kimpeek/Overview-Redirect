f6b290540b08bbee45d6f8d0c675c34cd59ac57c
KimPeek <kimpeek@users.noreply.github.com>
2017-09-27 05:50:58 -0500

    Update overview_redirect.user.js

--- overview_redirect.user.js
+++ overview_redirect.user.js
@@ -1,21 +1,38 @@
 // ==UserScript==
 // @name         Overview Redirect
 // @name:en      Overview Redirect
-// @version      3.1
+// @version      4.0
 // @description  Change all username urls to point to overviews instead of profiles
 // @namespace    Overview Redirect
 // @author       https://github.com/kimpeek
 // @include      https://www.reddit.com*
 // ==/UserScript==
 
-var user_link = new RegExp("(\/u\/|\/user\/)[A-Za-z0-9_-]+\/?$");
-var user_name = new RegExp("(\/u\/|\/user\/)[A-Za-z0-9_-]+");
+// Define the regex for a username link
+var username_link = new RegExp("(\/u\/|\/user\/)[A-Za-z0-9_-]+\/?$");
+// Gather all the links on the page
 var anchors = document.getElementsByTagName("a");
+// Transform the HTML Collection to an array
+var list = Array.from(anchors);
 
-for (var i = 0; i < anchors.length; i++) {
-    if (user_link.test(anchors[i].href)){
-        var user = anchors[i].href.match(user_name)[0];
-        var overview = "https://www.reddit.com" + user + "/overview";
-        anchors[i].href = overview;
+// Find all links that point to a username
+// and add 'overview' to the end
+function find_username_links(item, index){
+    if (item.href.match(username_link)){
+        item.href = change_username_link(item.href);
     }
 }
+
+// Conditionally add 'overview' to the link
+function change_username_link(link){
+    if (link.endsWith("/")){
+        link = link + "overview";
+    }
+    else {
+        link = link + "/overview";
+    }
+    return link
+}
+
+// Execute the primary functionality
+list.forEach(find_username_links);
