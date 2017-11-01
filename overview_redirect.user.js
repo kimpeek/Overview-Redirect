3af45068ca3f9a755f478f03f499186b6a14064c
Brennan <brennanmagee1@gmail.com>
2017-11-01 22:45:06 +0000

    Refactor using event listeners
    Enables use on RES' Never Ending Reddit

--- overview_redirect.user.js
+++ overview_redirect.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Overview Redirect
 // @name:en      Overview Redirect
-// @version      4.0
+// @version      4.1
 // @description  Change all username urls to point to overviews instead of profiles
 // @namespace    Overview Redirect
 // @author       https://github.com/kimpeek
@@ -10,29 +10,41 @@
 
 // Define the regex for a username link
 var username_link = new RegExp("(\/u\/|\/user\/)[A-Za-z0-9_-]+\/?$");
-// Gather all the links on the page
-var anchors = document.getElementsByTagName("a");
-// Transform the HTML Collection to an array
-var list = Array.from(anchors);
+// Choose the page to replace with
+//   Must be valid www.reddit.com/user/<USER>/*
+//   eg. 'overview' or 'submitted'
+var page_to_link = "submitted";
 
 // Find all links that point to a username
-// and add 'overview' to the end
+// and add 'page_to_link' to the end
 function find_username_links(item, index){
     if (item.href.match(username_link)){
         item.href = change_username_link(item.href);
     }
 }
 
-// Conditionally add 'overview' to the link
+// Conditionally add 'page_to_link' to the link
 function change_username_link(link){
     if (link.endsWith("/")){
-        link = link + "overview";
+        link = link + page_to_link;
     }
     else {
-        link = link + "/overview";
+        link = link + "/" + page_to_link;
     }
-    return link
+    return link;
 }
 
-// Execute the primary functionality
-list.forEach(find_username_links);
+// Wrapper for main functionality
+function take_action(msg){
+    // Gather all the links on the page
+    var anchors = document.getElementsByTagName("a");
+    // Transform the HTML Collection to an array
+    var list = Array.from(anchors);
+    // Execute the primary functionality
+    list.forEach(find_username_links);
+}
+
+// Normal reddit and RES first page
+window.addEventListener ("load", take_action());
+// Never Ending Reddit
+window.addEventListener ("neverEndingLoad", function(){take_action();});
